86d23d39b5a3f573ecc69c8489fef11b62946174
SpiralMoon <mjh1660@gmail.com>
2017-02-05 07:27:48 +0900

    1st last

--- D3S/src/main/webapp/WEB-INF/resources/js/search.js
+++ D3S/src/main/webapp/WEB-INF/resources/js/search.js
@@ -18,7 +18,7 @@ function addKeyword(event) {
 		console.log(keywordList);
 
 		cnt++;
-		count.value = cnt;
+		count.setAttribute("value", cnt);
 
 		keywordList.innerHTML +=
 		'<input name="data' + cnt + //데이터
@@ -33,9 +33,32 @@ function addKeyword(event) {
 		'</div>';
 
 		inputKeyword.value = "";
-	}	
+	}
 }
 
-function search() {
+function showList() {
+	var studentList = document.getElementById('studentList');
 
+	if (studentList.innerHTML == "") {
+		studentList.innerHTML +=
+		'<thead>' +
+		'<tr>' +
+		'<th data-field="name">이름</th>' +
+		'<th data-field="school_name">학교</th>' +
+		'<th data-field="school_name">학년</th>' +
+		'<th data-field="school_name">반</th>' +
+		'<th data-field="school_name">번호</th>' +
+		'</tr>' +
+		'</thead>' +
+		'<tbody>';
+	}
+	//학생 리스트 출력 (학생 수 많큼)
+	// <tr>
+	// 	            <td>Alvin</td>
+	// 	            <td>Alvin</td>
+	// 	            <td>Eclair</td>
+	// 	            <td>$0.87</td>
+	// 	            <td>$7.00</td>
+	// 	          </tr>
+	studentList.innerHTML += '</tbody>';
 }
